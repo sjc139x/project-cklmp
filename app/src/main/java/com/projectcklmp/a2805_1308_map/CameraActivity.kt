5980699404e319a9b331f110b31a8ef509611bfa
aprilw00t <lemonrabssister@gmail.com>
2019-05-30 10:42:58 +0100

    camera comment

--- app/src/main/java/com/projectcklmp/a2805_1308_map/CameraActivity.kt
+++ app/src/main/java/com/projectcklmp/a2805_1308_map/CameraActivity.kt
@@ -1,8 +1,14 @@
 package com.projectcklmp.a2805_1308_map
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.annotation.Nullable
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log
+import android.view.View
+import com.google.android.gms.tasks.OnSuccessListener
 
 import kotlinx.android.synthetic.main.activity_camera.*
 
@@ -20,3 +26,30 @@ class CameraActivity : AppCompatActivity() {
     }
 
 }
+
+
+//fun captureVideo(view: View) {//this is the onclick
+//    val videoIntent =
+//        Intent(MediaStore.ACTION_VIDEO_CAPTURE)//starts the capturevideo intent, makes a request to the camera2 api
+//    if (videoIntent.resolveActivity(getPackageManager()) != null) { //
+//        startActivityForResult(videoIntent, VIDEO_REQUEST)
+//    }
+//
+//}
+//
+//
+//
+//@Override
+//protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data ) {
+//    if (requestCode == VIDEO_REQUEST && resultCode == Activity.RESULT_OK) {
+//        if (data != null && data!!.getData() != null) {
+//            mStorageRef = FirebaseStorage.getInstance().getReference()
+//
+//            val file = data!!.getData()
+//            val riversRef = storageRef.child("images/rivers.jpg")
+//            riversRef.putFile(file).addOnSuccessListener(OnSuccessListener<Any> {
+//                // Get a URL to the uploaded content
+//                Log.d("ij", "ijoj")
+//            })
+//        }
+
