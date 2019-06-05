606d240e737e1e24373f1377e77d39bede90f4bb
Jonny McGarty <jon.mcgarty@gmail.com>
2019-06-05 21:22:20 +0100

    Add friends updates firebase database correctly

--- app/src/main/java/com/projectcklmp/a2805_1308_map/FriendsActivity.kt
+++ app/src/main/java/com/projectcklmp/a2805_1308_map/FriendsActivity.kt
@@ -16,11 +16,15 @@ class FriendsActivity : AppCompatActivity() {
     private lateinit var addFriendText: String
     private lateinit var addFriendButton: Button
     private lateinit var userId: String
+    private lateinit var friendId: String
+    private lateinit var username: String
     private lateinit var dbRef: FirebaseDatabase
-    private lateinit var userRef: DatabaseReference
+    private lateinit var userFriendsRef: DatabaseReference
     private lateinit var friendsRef: DatabaseReference
     private lateinit var recyclerView: RecyclerView
     private lateinit var auth: FirebaseAuth
+    private lateinit var lookupRef: DatabaseReference
+
 
     companion object {
         var FRIENDS_LIST: MutableList<String> = mutableListOf()
@@ -33,15 +37,19 @@ class FriendsActivity : AppCompatActivity() {
 
             dbRef = FirebaseDatabase.getInstance()
             auth = FirebaseAuth.getInstance()
-            userId = auth.currentUser!!.uid
-            friendsRef = dbRef.reference.child("users").child(userId).child("friends")
+            userId = auth!!.currentUser!!.uid
+            friendId = ""
+            username = ""
+            userFriendsRef = dbRef.reference.child("users").child(userId).child("friends")
+            friendsRef = dbRef.reference.child("users")
+            lookupRef = dbRef.reference.child("lookup")
             recyclerView = findViewById(R.id.friends_list)
             addFriendInput = findViewById(R.id.add_friend_input_field)
             addFriendButton = findViewById(R.id.add_friend_button)
 
             addFriendButton.setOnClickListener{
                 addFriendText = addFriendInput.text.toString()
-                addFriend()
+                addFriend(addFriendText)
             }
 
             getFriends()
@@ -49,9 +57,35 @@ class FriendsActivity : AppCompatActivity() {
     }
 
 
-    private fun addFriend() {
+    private fun addFriend(addFriendText: String) {
+        val valueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot){
+                friendId = dataSnapshot.child(addFriendText).value.toString()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("FriendsActivity", databaseError.message)
+            }
+        }
+
+        lookupRef.addListenerForSingleValueEvent(valueEventListener)
+
+        val usernameEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot){
+                username = dataSnapshot.child(userId).child("username").value.toString()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("FriendsActivity", databaseError.message)
+            }
+        }
+
+        friendsRef.addListenerForSingleValueEvent(usernameEventListener)
+
         if (addFriendText != null) {
-            friendsRef.child(addFriendText).setValue("pending")
+            Log.d("username", username)
+            Log.d("friendId", friendId)
+            friendsRef.child(friendId).child("friends").child(username).setValue(userId)
         }
     }
 
@@ -64,7 +98,7 @@ class FriendsActivity : AppCompatActivity() {
 
                     friendsSnapshot.forEach {
                         if (it.value == "accepted") FRIENDS_LIST.add(it.key.toString())
-                        if (it.value === "pending") REQUEST_LIST.add(it.key.toString())
+                        if (it.value != "accepted") REQUEST_LIST.add(it.key.toString())
                     }
 
                 recyclerView.layoutManager = LinearLayoutManager(this@FriendsActivity)
@@ -78,11 +112,9 @@ class FriendsActivity : AppCompatActivity() {
 
         }
 
-        friendsRef.addListenerForSingleValueEvent(valueEventListener)
+        userFriendsRef.addListenerForSingleValueEvent(valueEventListener)
 
     }
-
-
 
 }
 
