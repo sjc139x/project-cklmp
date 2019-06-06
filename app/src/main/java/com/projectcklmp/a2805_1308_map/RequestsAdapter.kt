1c2a9ce8655fb9c85bdcbd88e893e0cdbbec775b
TALLuff <talluff@gmail.com>
2019-06-06 09:22:42 +0100

    Accept friend requests

--- app/src/main/java/com/projectcklmp/a2805_1308_map/RequestsAdapter.kt
+++ app/src/main/java/com/projectcklmp/a2805_1308_map/RequestsAdapter.kt
@@ -1,12 +1,16 @@
 package com.projectcklmp.a2805_1308_map
 
+import android.renderscript.Sampler
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.request_cell.view.*
 
 class RequestsAdapter: RecyclerView.Adapter<RequestsAdapter.CustomViewHolder>() {
@@ -29,7 +33,54 @@ class RequestsAdapter: RecyclerView.Adapter<RequestsAdapter.CustomViewHolder>() 
         holder.view.friend_request_decline_button.setOnClickListener {
             deleteRequest(requestToDisplay)
         }
+        holder.view.friend_request_accept_button.setOnClickListener {
+            acceptRequest(requestToDisplay)
+        }
         holder.view.request_username.text = requestToDisplay
+    }
+
+    private fun acceptRequest(requestToDisplay: String) {
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+        var friendId = ""
+        var username = ""
+        val dbRef = FirebaseDatabase.getInstance().reference
+        val usersRef = dbRef.child("users")
+        val lookupRef = dbRef.child("lookup")
+
+        Log.d("RequestsAdapter", "requestToDisplay TOP ---> $requestToDisplay")
+
+
+        val usersEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                username = dataSnapshot.child(userId).child("username").value.toString()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("FriendsActivity", databaseError.message)
+            }
+        }
+
+        usersRef.addListenerForSingleValueEvent(usersEventListener)
+
+
+        val valueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot){
+                friendId = dataSnapshot.child(requestToDisplay).value.toString()
+                usersRef.child(friendId).child("friends").child(username).setValue("accepted")
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("FriendsActivity", databaseError.message)
+            }
+        }
+
+        lookupRef.addListenerForSingleValueEvent(valueEventListener)
+
+
+        Log.d("RequestsAdapter", "requestToDisplay BOTTOM ---> $requestToDisplay")
+
+        usersRef.child(userId).child("friends").child(requestToDisplay).setValue("accepted")
+
     }
 
     private fun deleteRequest(declinedRequest: String) {
